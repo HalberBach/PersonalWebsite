139ff51328992e4cff0ec5e0a5a2804626d049a5
HalberBach <nilsjogie@gmail.com>
2026-07-10 18:40:23 +0200

    hero headline v1

---
+++ src/components/Hero/HeroText.svelte
@@ -0,0 +1,17 @@
+<div
+  class="hero-text-wrapper uppercase font-bebas mx-auto w-full max-w-5xl text-[6rem] sm:text-[10rem] md:text-[16rem] leading-[0.73] tracking-[-0.02em] transform scale-y-[1.7]"
+>
+  <p class="hero-line m-0 text-left">SOFTWARE</p>
+  <p class="hero-line m-0 text-right">DEVELOPER</p>
+</div>
+
+<style>
+  .hero-text-wrapper {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .hero-line {
+    width: 100%;
+  }
+</style>
